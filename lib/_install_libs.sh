e08313930f7cce5e4e8431e0c67d9aa77558acd2
knopper <knopper@holmes.nl>
2015-01-27 16:06:39 +0100

    Added install libs executable for *nix os

---
+++ lib/_install_libs.sh
@@ -0,0 +1,13 @@
+@echo off
+
+echo Installing Aspose libraries...
+
+
+mvn install:install-file -Dpackaging=jar -Dversion=8.3.0   -DgroupId=com.aspose        -DartifactId=aspose-cells    		-Dfile=aspose.cells-8.3.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=3.0.0 	-DgroupId=com.aspose        -DartifactId=aspose-diagram    		-Dfile=aspose.diagram-3.0.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=4.7.0.0 -DgroupId=com.aspose        -DartifactId=aspose-email    		-Dfile=aspose.email-4.7.0.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=2.4.0 	-DgroupId=com.aspose        -DartifactId=aspose-imaging  		-Dfile=aspose.imaging-2.4.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=9.7.0   -DgroupId=com.aspose        -DartifactId=aspose-pdf      		-Dfile=aspose.pdf-9.7.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=14.8.0  -DgroupId=com.aspose        -DartifactId=aspose-slides   		-Dfile=aspose.slides-14.8.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=7.3.0	-DgroupId=com.aspose        -DartifactId=aspose-tasks    		-Dfile=aspose.tasks-7.3.0.jar
+mvn install:install-file -Dpackaging=jar -Dversion=14.10.0 -DgroupId=com.aspose        -DartifactId=aspose-words    		-Dfile=aspose.words-14.10.0.jar
